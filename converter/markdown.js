2c2dc71b9928c0440ed96603bfa160bd7ea3167f
Aamir <amirouakhir467@gmail.com>
2026-01-07 22:50:18 +0100

    add horizontal line logic to parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -1,20 +1,58 @@
- var parseHeadline = function(str) {
-   var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
-   var stra = [];
-   while ((stra = headlineRegExp.exec(str)) !== null) {
-     var count = stra[1].length;
-     var content = stra[2].trim();
-     str = str.replace(stra[0], '<h' + count + '>' + content + '</h' + count + '>' + '\n');
-   }
-   return str;
- }
+var parseHeadline = function(str) {
+  var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
+  var stra = [];
+  while ((stra = headlineRegExp.exec(str)) !== null) {
+    var count = stra[1].length;
+    var content = stra[0].slice(stra[1].length).replace(/\s+$/, '');
+    var contentWithLeading = (content.charAt(0) === ' ') ? content : (' ' + content);
+    str = str.replace(stra[0], '<h' + count + '>' + contentWithLeading + '</h' + count + '>' + '\n');
+  }
+  return str;
+};
+
+var parseHorizontalLine = function (str) {
+  // match a line containing 3 or more of -, * or _ with optional spaces between
+  var hrRegExp = /^[ \t]*([*_-])(?:[ \t]*\1){2,}[ \t]*$/gm;
+  var match = null;
+  while ((match = hrRegExp.exec(str)) !== null) {
+    str = str.replace(match[0], '\n<hr/>\n');
+  }
+  return str;
+};
 
 var markdown = {
   parse: function (str, strict) {
     'use strict';
-    str = parseHeadline(str);
-    return str;
-  }
-};
+    var parseHeadline = function(str) {
+      var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
+      var stra = [];
+      while ((stra = headlineRegExp.exec(str)) !== null) {
+        var count = stra[1].length;
+        var content = stra[0].slice(stra[1].length).replace(/\s+$/, '');
+        var contentWithLeading = (content.charAt(0) === ' ') ? content : (' ' + content);
+        str = str.replace(stra[0], '<h' + count + '>' + contentWithLeading + '</h' + count + '>' + '\n');
+      }
+      return str;
+    };
 
-module.exports = markdown;
+    var parseHorizontalLine = function (str) {
+      // match a line containing 3 or more of -, * or _ with optional spaces between
+      var hrRegExp = /^[ \t]*([*_-])(?:[ \t]*\1){2,}[ \t]*$/gm;
+      var match = null;
+      while ((match = hrRegExp.exec(str)) !== null) {
+        str = str.replace(match[0], '\n<hr/>\n');
+      }
+      return str;
+    };
+
+    var markdown = {
+      parse: function (str, strict) {
+        'use strict';
+        str = parseHorizontalLine(str);
+        str = parseHeadline(str);
+        return str;
+      }
+    };
+
+    module.exports = markdown;
+      str = parseHorizontalLine(str);
