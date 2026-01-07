04cad8df1918c40d2447bfc19bfd7a7ab31adfda
Aamir <amirouakhir467@gmail.com>
2026-01-07 21:35:37 +0100

    Fix: h1 parser and Windows script compatibility

--- converter/markdown.js
+++ converter/markdown.js
@@ -1,9 +1,10 @@
  var parseHeadline = function(str) {
-   var headlineRegExp =  /^(\#{1,6})([^\#\n]+)$/m;
+   var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
    var stra = [];
    while ((stra = headlineRegExp.exec(str)) !== null) {
      count = stra[1].length;
-     str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>' + '\n');
+     var content = stra[2].replace(/\s+$/, '');
+     str = str.replace(stra[0], '<h' + count + '>' + content + '</h' + count + '>' + '\n');
    }
    return str;
  }
